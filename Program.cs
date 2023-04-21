334247d9df780b13dfe152cfef1c763e7355b4d6
TrickSoLogic <118069084+TrickSoLogic@users.noreply.github.com>
2023-04-21 22:29:35 +0530

    commits

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
+using System.Numerics;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World! Welcome! Happy Coding - Subscribe -->> https://www.youtube.com/TrickSoLogic/ ");
 
 app.Run();
+    //< h2 > Please dont mis use these code as its copyright protected and you will be seriously fined for any reuse of this project or from any of my GIT project</h2>
